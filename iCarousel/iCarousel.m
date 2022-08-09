606fa47aadd41ba7a9ebfba483cd720fda5dbc51
huangqianlong <huangqianlong@drojian.dev>
2022-08-09 18:11:37 +0800

    itemViewAtPoint: fun update

--- iCarousel/iCarousel.m
+++ iCarousel/iCarousel.m
@@ -380,7 +380,7 @@ NSComparisonResult compareViewDepth(UIView *view1, UIView *view2, iCarousel *sel
 {
     for (UIView *view in [[[_itemViews allValues] sortedArrayUsingFunction:(NSInteger (*)(id, id, void *))compareViewDepth context:(__bridge void *)self] reverseObjectEnumerator])
     {
-        if ([view.superview.layer hitTest:point])
+        if (CGRectContainsPoint(view.superview.frame, point))
         {
             return view;
         }
